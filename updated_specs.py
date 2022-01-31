d84b7ef4bf002bed0a3dab07a104e6f0fd9d2122
ginger <gtsueng@gmail.com>
2022-01-31 11:52:43 -0800

    add owl

--- updated_specs.py
+++ updated_specs.py
@@ -31,6 +31,7 @@ def check_context_url(spec_json):
     bioschemasUrl = "https://discovery.biothings.io/view/bioschemas/"
     contextInfo["bioschemas"]=bioschemasUrl
     contextInfo["dct"] = "http://purl.org/dc/terms/"
+    contextInfo["owl"] = "http://www.w3.org/2002/07/owl#"
     return(contextInfo)
 
 def update_subclass(spec_list,eachurl,cleantext):
