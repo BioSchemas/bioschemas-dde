56158dfc9b06941905e88b517651fd5f5253b80e
unknown <gtsueng@gmail.com>
2022-11-11 01:27:50 -0800

    fix auto update script

--- update_repo_tables.py
+++ update_repo_tables.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import os
 import pathlib
-from src.metatables.py import *
+from src.metatables import *
 
 #### Main
 script_path = pathlib.Path(__file__).parent.absolute()
